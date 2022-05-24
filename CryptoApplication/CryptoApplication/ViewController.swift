63be4cf6f2e79427a8e97d36c20c528aef91e66e
annesh0 <67394141+annesh0@users.noreply.github.com>
2022-05-23 19:09:53 -0700

    Created New App

---
+++ CryptoApplication/CryptoApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoApplication
+//
+//  Created by Annesh Ghosh Dastidar on 5/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
