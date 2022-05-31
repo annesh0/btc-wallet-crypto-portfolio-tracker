8b4a91e3ec5d29be0085e0b57e81829c8044b02d
annesh0 <67394141+annesh0@users.noreply.github.com>
2022-05-31 13:12:41 -0700

    Added asset class

---
+++ CryptoApplication/assets.swift
@@ -0,0 +1,14 @@
+//
+//  assets.swift
+//  CryptoApplication
+//
+//  Created by Annesh Ghosh Dastidar on 5/31/22.
+//
+
+import Foundation
+
+var allAssets: [Int:String] = [:] // Lists all asset names mapped to key
+
+var heldAssets: [Int:Int] = [:] //Maps key of asset to amount of asset held
+
+
