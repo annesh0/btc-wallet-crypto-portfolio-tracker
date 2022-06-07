62868b83b6d900032873e481048c9231953d83ba
annesh0 <67394141+annesh0@users.noreply.github.com>
2022-06-06 21:37:08 -0700

    Demo api call

---
+++ CryptoApplication/CryptoApplication/NetworkManager.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkManager.swift
+//  CryptoApplication
+//
+//  Created by Annesh Ghosh Dastidar on 6/6/22.
+//
+
+import Alamofire
+import Foundation
+
+class NetworkManager {
+    static let host = "https://ios-course-message-board.herokuapp.com"
+    
+    static func getAllPosts(completion: @escaping ([Post]) -> Void) {
+        let endpoint = "\(host)/posts/all/"
+        //let params: [String:String] = [
+          //  "key": "value"
+        //]
+        AF.request(endpoint, method: .get).validate().responseData { response in
+            //process response
+            switch(response.result) {
+            case .success(let data):
+                let jsonDecoder = JSONDecoder()
+                if let userResponse = try? jsonDecoder.decode([Post].self, from: data) {
+                    completion(userResponse)
+                } else {
+                    print("Failed Decoding")
+                }
+                print(data)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
---
+++ CryptoApplication/CryptoApplication/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  networking
+//
+//  Created by Justin Ngai on 3/29/22.
+//
+
+// MARK: TODO - Fill this file in with models to properly decode the JSON responses from the server
+
+struct Post : Decodable {
+    //match lines of json response
+    let id: String
+    let title: String
+    let body: String
+    let poster: String
+    let timeStamp: String
+}
