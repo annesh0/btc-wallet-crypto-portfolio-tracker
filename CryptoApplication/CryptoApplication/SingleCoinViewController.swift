cfcadf6cb8c48d7a81c48544282773c9e41c3038
DylanMcCreesh <dylanmccreesh@gmail.com>
2022-06-08 17:19:31 -0400

    Fixed Single Coin View in crypto app

--- CryptoApplication/CryptoApplication/SingleCoinViewController.swift
+++ CryptoApplication/CryptoApplication/SingleCoinViewController.swift
@@ -8,25 +8,60 @@
 import UIKit
 class SingleCoinViewController: UIViewController{
     
-    var assetsLabel = UILabel()
+    var logoImage = UIImageView()
+    var amountUSDLabel = UILabel()
+    var amountCoinAndChangeLabel = UILabel()
+    var dayButton = UIButton()
+    var weekButton = UIButton()
+    var monthButton = UIButton()
+    var yearButton = UIButton()
+    var fiveYearButton = UIButton()
+    var graphImage = UIImageView()
     weak var parentController: ViewController?
     weak var parentCoin: Coin?
     
     override func viewDidLoad() {
         title = parentCoin!.name
         view.backgroundColor = .white
-        assetsLabel.text = "Assets"
-        assetsLabel.font = .systemFont(ofSize: 30, weight: .bold)
-        assetsLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(assetsLabel)
+        
+        logoImage.image = parentCoin!.image
+        logoImage.contentMode = .scaleAspectFit
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoImage)
+
+
+        amountCoinAndChangeLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        amountCoinAndChangeLabel.textColor = .lightGray
+        amountCoinAndChangeLabel.text = "\(parentCoin!.getCurrencyForm(amount: parentCoin!.amountCoin))  \(parentCoin!.symbol)   \(parentCoin!.getRoundedPercentage(amount: parentCoin!.percentChnage))"
+        amountCoinAndChangeLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(amountCoinAndChangeLabel)
+        
+        amountUSDLabel.font = .systemFont(ofSize: 28, weight: .bold)
+        amountUSDLabel.text = "$\(parentCoin!.getCurrencyForm(amount: parentCoin!.amountUSD))"
+        amountUSDLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(amountUSDLabel)
+        
         setupConstraints()
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            assetsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            assetsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10)
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImage.heightAnchor.constraint(equalToConstant: 75),
+            logoImage.widthAnchor.constraint(equalToConstant: 75)
         ])
+        
+        NSLayoutConstraint.activate([
+            amountUSDLabel.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 15),
+            amountUSDLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            amountCoinAndChangeLabel.topAnchor.constraint(equalTo: amountUSDLabel.bottomAnchor, constant: 10),
+            amountCoinAndChangeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+
     }
 }
 
